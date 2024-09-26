11e3171db5c280d47d1fa3bf101ba8f86b7d1040
MM2K24 <moraismatheus666007@gmail.com>
2024-09-26 15:45:14 -0300

    update

--- main.py
+++ main.py
@@ -12,8 +12,33 @@ def menu():
     print('4 - Sair')
     return input('Escolha uma opção: ')
 
+#Código do município = SG_UE
+#Código do cargo = CD_CARGO
+#Nome do candidato = NM_CANDIDATO
+#Nome do candidato na urna = NM_URNA_CANDIDATO
+#Número do candidato = NR_CANDIDATO
+#Número do partido = NR_PARTIDO
+
 while True:
-    opcao = menu()
-    match opcao: 
-        case 1:
-            
+    opcao = menu(opcao_1)
+    if opcao == "opcao_1":
+        def listar_candidatos(csv):
+            codigo_municipio = input('Digite o código do município: ')
+            codigo_cargo = input('Digite o código do cargo: ') 
+            try:
+                codigo_municipio = int(codigo_municipio)
+                codigo_cargo = int(codigo_cargo)
+            except ValueError:
+                    print('Código inválido. Insira valores númericos.')
+
+            candidatos_filtrados = csv[(csv['SG_UE'] == codigo_municipio) & (csv["CD_CARGO"] == codigo_cargo)]
+            if candidatos_filtrados.empty:
+                print('Nenhum candidato encontrado para o município e cargo fornecidos.')
+            else:
+                print('Candidatos encontrados: ')
+                for index, candidato in candidatos_filtrados.iterrows():
+                    print(f'Nome: {candidato["NM_CANDIDATO"]}')
+                    print(f'Nome na Urna: {candidato["NM_URNA_CANDIDATO"]}')
+                    print(f'Número: {candidato["NR_CANDIDATO"]}')
+                    print(f'Partido: {candidato["NR_PARTIDO"]}')
+                    print('-'* 30)
